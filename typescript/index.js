3e9cc3b0f6bffcc718f5686bdc610b75fd405628
ranganathnk <ranganathnkulkarni@gmail.com>
2022-10-17 16:54:37 +0530

    initial

---
+++ typescript/index.js
@@ -0,0 +1,7 @@
+function addition(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 3110;
+var number2 = 100;
+var result = addition(number1, number2);
+console.log(result);
---
+++ typescript/index.ts
@@ -0,0 +1,9 @@
+function addition(n1: number,n2:number){
+    return n1+n2;
+}
+
+const number1=3110;
+const number2=100;
+
+const result=addition(number1,number2);
+console.log(result);
