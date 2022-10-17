a972687804c39047d79441bb31c11cb374bed278
ranganathnk <ranganathnkulkarni@gmail.com>
2022-10-17 17:26:41 +0530

    nested objects added

--- typescript/index.js
+++ typescript/index.js
@@ -8,3 +8,14 @@ var person = {
     age: 25
 };
 console.log(person.age);
+//Nested objects
+var product = {
+    id: "abc1",
+    price: 12.99,
+    tags: ["great-offer", "hot-and-new"],
+    details: {
+        title: "Red Carpet",
+        description: "A great carpet - almost brand-new!"
+    }
+};
+console.log(product.details.title);
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,11 +1,31 @@
 //Objects
+
 // const person:{
 //     name:string;
 //     age:number;
 // }={
-    const person={
-    name:"Sachin",
-    age:25
+const person = {
+  name: "Sachin",
+  age: 25,
 };
-
 console.log(person.age);
+
+//Nested objects
+const product: {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  };
+} = {
+  id: "abc1",
+  price: 12.99,
+  tags: ["great-offer", "hot-and-new"],
+  details: {
+    title: "Red Carpet",
+    description: "A great carpet - almost brand-new!",
+  },
+};
+console.log(product.details.title);
